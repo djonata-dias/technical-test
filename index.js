27ebd1801a30acf65d4b4a64c4a8d20389ca0b0e
Djonata de Souza Dias <djonata.dias@dotgroup.com.br>
2021-08-06 20:37:41 -0300

    I apllyed the routes file

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT;
+const routes = require('./routes');
 
-
+app.use('/api', routes)
 
 app.listen(PORT, () => {
   console.log(`Listening port: ${PORT}`);
