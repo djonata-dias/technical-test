fe995ac4fca3994a100f54431d6d3945442d6617
Djonata de Souza Dias <djonata.dias@dotgroup.com.br>
2021-08-10 09:29:13 -0300

    I created the post request to change currencys values of the currency.json file

--- routes.js
+++ routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
-const router = express.Router();
+const fs = require('fs');
 const loginController = require('./controllers/loginController');
 const auth = require('./middlewares/auth');
 const { fetchAPI } = require('./utils');
 const jsonData = require('./currencies.json');
+const router = express.Router();
 
 router.post('/login', loginController);
 
-router.get('/crypto/btc', async (req, res) => {
+router.get('/crypto/btc', async (_req, res) => {
   try {
     const { time, disclaimer, bpi } = await fetchAPI();
 
@@ -33,13 +34,34 @@ router.get('/crypto/btc', async (req, res) => {
       },
       "BTC": bpi.BTC
     };
-    const reqObj = { time, disclaimer, bpi: newBpi };
 
-    res.status(200).json(reqObj);
+    res.status(200).json({ time, disclaimer, bpi: newBpi });
   } catch (error) {
-    console.error(error)
+    res.status(500).json({ messsage: error });
   }
 
 })
 
+router.post('/crypto/btc', auth,(req, res) => {
+  const { currency, value } = req.body;
+  const currencys = ['BRL', 'EUR', 'CAD'];
+
+  if (!currency || currencys.indexOf(currency) === -1 || typeof currency !== 'string') {
+    return res.status(400).json({ message: "Moeda inválida" });
+  }
+  if (!value || typeof value !== 'number') {
+    return res.status(400).json({ message: "Valor inválido" });
+  }
+
+  jsonData[currency] = new Intl.NumberFormat('en-IN', { maximumFractionDigits: 3, minimumFractionDigits: 3 }).format(value)
+
+  fs.writeFile("currencies.json", JSON.stringify(jsonData), 'utf8', function (error) {
+    if (error) {
+      return res.status(500).json({ messsage: error });
+    }
+
+    res.status(200).json({ messsage: "Valor alterado com sucesso!" });
+  });
+
+})
 module.exports = router;
