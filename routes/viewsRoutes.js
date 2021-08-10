7abe56d64061f7cc421f4d726f0a8e0dc2ad313b
Djonata de Souza Dias <djonata.dias@dotgroup.com.br>
2021-08-10 17:43:18 -0300

    creating the routes where i'll render the views pages

---
+++ routes/viewsRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+
+
+router.get('/login', async (_req, res) => {
+  try {
+    res.status(200).render('login');
+  } catch (error) {
+    res.status(500).json({ messsage: error });
+  }
+})
+
+
+
+module.exports = router;
