09cc323dc662764a069859e29bd4136d82ead62e
Djonata de Souza Dias <djonata.dias@dotgroup.com.br>
2021-08-10 17:42:24 -0300

    A adjust in the controller response

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -3,16 +3,17 @@ const emailValidate = require('valid-email');
 
 function loginController(req, res) {
   const { email, password } = req.body;
-
   if (emailValidate(email) && password.length >= 6) {
     const id = 1;
     const token = jwt.sign({ id }, process.env.SECRET_TOKEN, {
       expiresIn: 900 // expires in 15 min
     });
-    return res.status(200).json({auth: true, token})
+    res.status(200)
+    console.log(JSON.stringify(req.body));
 
+    return res.json({auth: true, token})
   }
-  return res.status(400).json({ message: "Campos inválidos" });
+  return res.status(400).json({ message: "Campos inválidos!" });
 }
 
 module.exports = loginController;
