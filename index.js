c1c3e28637e6f4fa0547e129b9a98a9f0c751018
Djonata de Souza Dias <djonata.dias@dotgroup.com.br>
2021-08-10 08:53:35 -0300

    I had added the body parser to json of the express

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 
 const express = require('express');
-const app = express();
-
-const PORT = process.env.PORT;
 const routes = require('./routes');
 
+const app = express();
+app.use(express.json())
+
+const PORT = process.env.PORT;
+
 app.use('/api', routes)
+
 
 app.listen(PORT, () => {
   console.log(`Listening port: ${PORT}`);
